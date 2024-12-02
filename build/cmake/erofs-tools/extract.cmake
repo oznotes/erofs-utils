fd18360ad76b6631f8802c00d7c025e9766c03fc
Oz <oz.ozgur@ymail.com>
2024-12-02 16:39:06 +0800

    Update extract.cmake

--- build/cmake/erofs-tools/extract.cmake
+++ build/cmake/erofs-tools/extract.cmake
@@ -1,5 +1,4 @@
 ###############################------extract.erofs------###############################
-# Define both DLL and executable targets
 set(TARGET_extract_dll "erofs_extract")
 set(TARGET_extract "extract.erofs")
 
@@ -15,20 +14,27 @@ list(FILTER extract_lib_srcs EXCLUDE REGEX ".*main\\.cpp$")
 add_library(${TARGET_extract_dll} SHARED ${extract_lib_srcs})
 target_include_directories(${TARGET_extract_dll} 
     PUBLIC
-        "${TARGET_SRC_DIR}/include"    # Public headers location
+        "${TARGET_SRC_DIR}/include"
     PRIVATE
         "${TARGET_SRC_DIR}"
         ${common_headers}
 )
 target_link_libraries(${TARGET_extract_dll} ${common_static_link_lib})
-target_compile_options(${TARGET_extract_dll} PRIVATE ${common_compile_flags} "-Wno-unused-result")
-target_compile_definitions(${TARGET_extract_dll} PRIVATE EROFS_EXTRACT_EXPORTS)
+target_compile_options(${TARGET_extract_dll} PRIVATE 
+    ${common_compile_flags} 
+    "-Wno-unused-result"
+    "-fno-exceptions"  # Disable exceptions
+)
+target_compile_definitions(${TARGET_extract_dll} PRIVATE 
+    EROFS_EXTRACT_EXPORTS
+    _GNU_SOURCE
+)
 
-# Build the executable
+# Original executable build
 file(GLOB extract_exe_srcs "${TARGET_SRC_DIR}/*.cpp")
 add_executable(${TARGET_extract} ${extract_exe_srcs})
 target_include_directories(${TARGET_extract} PRIVATE
-        "${TARGET_SRC_DIR}/include"    # Headers location
+        "${TARGET_SRC_DIR}/include"
         ${common_headers}
 )
 target_link_libraries(${TARGET_extract} ${common_static_link_lib})
