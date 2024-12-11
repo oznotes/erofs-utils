97c10a2771e77490b849b0a5a36f10de19065965
Oz <oz.ozgur@ymail.com>
2024-12-11 16:31:30 +0800

    Update extract.cmake

--- build/cmake/erofs-tools/extract.cmake
+++ build/cmake/erofs-tools/extract.cmake
@@ -25,6 +25,9 @@ target_compile_options(${TARGET_extract_dll} PRIVATE
     "-Wno-unused-result"
     "-fno-exceptions"  # Disable exceptions
 )
+
+target_compile_options(${TARGET_extract_dll} PRIVATE -fvisibility=default)
+
 target_compile_definitions(${TARGET_extract_dll} PRIVATE 
     EROFS_EXTRACT_EXPORTS
     _GNU_SOURCE
