6717423dc4fc616a0cf901b2c427c9a09fbfb4b4
Oz <oz.ozgur@ymail.com>
2024-12-13 17:20:37 +0800

    Update erofs_extract_dll.h

--- extract/include/erofs_extract_dll.h
+++ extract/include/erofs_extract_dll.h
@@ -2,13 +2,22 @@
 #define EROFS_EXTRACT_DLL_H
 
 #include <stdint.h>
-
 #include "ExtractState.h"
 
-#ifdef EROFS_EXTRACT_EXPORTS
-#define EROFS_API __declspec(dllexport) __attribute__((cdecl))
+#ifdef _MSC_VER
+    #ifdef EROFS_EXTRACT_EXPORTS
+        #define EROFS_API __declspec(dllexport)
+    #else
+        #define EROFS_API __declspec(dllimport)
+    #endif
+    #define EROFS_CALL __cdecl
 #else
-#define EROFS_API __declspec(dllimport) __attribute__((cdecl))
+    #ifdef EROFS_EXTRACT_EXPORTS
+        #define EROFS_API __declspec(dllexport) __attribute__((cdecl))
+    #else
+        #define EROFS_API __declspec(dllimport) __attribute__((cdecl))
+    #endif
+    #define EROFS_CALL
 #endif
 
 #ifdef __cplusplus
@@ -23,13 +32,13 @@ typedef struct erofs_extract_options {
 } erofs_extract_options;
 
 // API Functions
-EROFS_API int __cdecl erofs_extract_init(const char* image_path);
-EROFS_API int __cdecl erofs_extract_set_outdir(const char* out_dir);
-EROFS_API int __cdecl erofs_extract_path(const char* target_path, bool recursive);
-EROFS_API int __cdecl erofs_extract_all(void);
-EROFS_API int __cdecl erofs_extract_set_options(const erofs_extract_options* options);
-EROFS_API const char* __cdecl erofs_extract_get_error(void);
-EROFS_API void __cdecl erofs_extract_cleanup(void);
+EROFS_API int EROFS_CALL erofs_extract_init(const char* image_path);
+EROFS_API int EROFS_CALL erofs_extract_set_outdir(const char* out_dir);
+EROFS_API int EROFS_CALL erofs_extract_path(const char* target_path, bool recursive);
+EROFS_API int EROFS_CALL erofs_extract_all(void);
+EROFS_API int EROFS_CALL erofs_extract_set_options(const erofs_extract_options* options);
+EROFS_API const char* EROFS_CALL erofs_extract_get_error(void);
+EROFS_API void EROFS_CALL erofs_extract_cleanup(void);
 
 #ifdef __cplusplus
 }
