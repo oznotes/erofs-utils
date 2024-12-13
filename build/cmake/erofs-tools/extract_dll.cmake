89efb0be3339c10f898650502d31d4901c642b20
Oz <oz.ozgur@ymail.com>
2024-12-13 19:06:41 +0800

    Update extract_dll.cmake

--- build/cmake/erofs-tools/extract_dll.cmake
+++ build/cmake/erofs-tools/extract_dll.cmake
@@ -1,53 +1,37 @@
 set(TARGET_extract_dll "erofs_extract")
-set(TARGET_SRC_DIR "${PROJECT_ROOT_DIR}/extract")
 
 add_library(${TARGET_extract_dll} SHARED
-    ${TARGET_SRC_DIR}/erofs_extract_dll.cpp
-    ${TARGET_SRC_DIR}/ExtractOperation.cpp
-    ${TARGET_SRC_DIR}/ExtractHelper.cpp
-    ${TARGET_SRC_DIR}/ErofsNode.cpp
-    ${TARGET_SRC_DIR}/ErofsHardlinkHandle.cpp
+    ${PROJECT_ROOT_DIR}/extract/erofs_extract_dll.cpp
+    ${PROJECT_ROOT_DIR}/extract/ExtractOperation.cpp
+    ${PROJECT_ROOT_DIR}/extract/ExtractHelper.cpp
+    ${PROJECT_ROOT_DIR}/extract/ErofsNode.cpp
+    ${PROJECT_ROOT_DIR}/extract/ErofsHardlinkHandle.cpp
 )
 
 target_include_directories(${TARGET_extract_dll} PRIVATE
-    "${TARGET_SRC_DIR}/include"
-    ${common_headers}
+    ${PROJECT_ROOT_DIR}/extract/include
     ${PROJECT_ROOT_DIR}/include
+    ${common_headers}
+)
+
+target_link_libraries(${TARGET_extract_dll} PRIVATE
+    erofs_static
+    ${common_static_link_lib}
 )
 
 target_compile_definitions(${TARGET_extract_dll} PRIVATE
-    EROFS_EXTRACT_EXPORTS=1
+    EROFS_EXTRACT_EXPORTS
     _FILE_OFFSET_BITS=64
     _LARGEFILE_SOURCE
     _LARGEFILE64_SOURCE
     CYGWIN
-    _DEBUG
-    _GNU_SOURCE
-)
-
-target_compile_options(${TARGET_extract_dll} PRIVATE
-    -fvisibility=hidden
-    -g
-    -O0
-    -Wall
-    -Wextra
-    -fno-exceptions
-    -fno-rtti
-    -fPIC
-)
-
-target_link_libraries(${TARGET_extract_dll} PRIVATE
-    ${common_static_link_lib}
-    erofs_static
-    iconv
 )
 
 set_target_properties(${TARGET_extract_dll} PROPERTIES
-    PREFIX ""
-    OUTPUT_NAME "cygerofs_extract"
-    LINK_FLAGS "-Wl,--enable-auto-import -Wl,--nxcompat -Wl,--no-undefined"
-    DEBUG_POSTFIX "d"
-    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/erofs-tools"
+    PREFIX "cyg"
+    OUTPUT_NAME "erofs_extract"
+    VERSION ${PROJECT_VERSION}
+    SOVERSION ${PROJECT_VERSION_MAJOR}
 )
 
 install(TARGETS ${TARGET_extract_dll}
